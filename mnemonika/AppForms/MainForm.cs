7627209cd39e58952f4d0057ed53ceb4967888d1
Игорь Шапкин <shapkin.igorek@gmail.com>
2026-01-16 13:18:32 +0300

    Пример работы с даными (MainForm)

--- mnemonika/AppForms/MainForm.cs
+++ mnemonika/AppForms/MainForm.cs
@@ -1,10 +1,17 @@
-﻿namespace mnemonika.AppForms
+﻿using mnemonika.AppModules;
+
+namespace mnemonika.AppForms
 {
     public partial class MainForm : ParentForm
     {
         public MainForm()
         {
             InitializeComponent();
+
+            using (var context = new Entities())
+            {
+                context.Auxi
+            }
         }
     }
 }
